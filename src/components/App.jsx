de5a5e5ed79e7bdbf79bdde1a2a4b220475d1dfd
MOOW4 <108760540+MOOW4@users.noreply.github.com>
2023-10-04 23:59:42 +0200

    implement react components

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+function App() {
+	return (
+		<div className="body">
+			<Heading />
+			<List />
+		</div>
+	);
+}
+
+export default App;
